7d80495031a4c641c345752b5a50386e5227d81d
danijelsokac92 <danijel.sokac92@gmail.com>
2020-08-23 10:14:59 +0200

    add task names

--- src/navloganalyzer/AppConstants.java
+++ src/navloganalyzer/AppConstants.java
@@ -5,4 +5,11 @@ public abstract class AppConstants {
         String RAW_FILES_ENTRY_LOCATION = "Raw Files";
         String CLEANED_FILES_LOCATION = "Cleaned Files";
     }
+    
+    public interface Tasks {
+        String UPLOAD_TASK = "upload";
+        String CLEAN_FILES_TASK = "clean files";
+        String REMOVE_ELEMENTS_TASK = "remove irrelevent elements";
+        String MAP_TO_JAVA_TASK = "map xml to java";
+    }
 }
