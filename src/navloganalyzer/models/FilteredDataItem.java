b67e3443a055398b1f78fa7f20874350548d6260
danijelsokac92 <danijel.sokac92@gmail.com>
2020-08-24 09:20:58 +0200

    introduce new class to hold relevant information for further analysis

---
+++ src/navloganalyzer/models/FilteredDataItem.java
@@ -0,0 +1,65 @@
+package navloganalyzer.models;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class FilteredDataItem {
+    
+    private String username;
+    private int eventId;
+    private String logonType;
+    private Date createdAt;
+    
+    public FilteredDataItem(XmlEvent event) {
+        this.username = event.getEventData().getTargetUserName();
+        this.eventId = event.getSystemData().getEventId();
+        this.logonType = event.getEventData().getLogonType();
+        this.createdAt = event.getSystemData().getTimeCreated().getSystemTime();
+    }
+    
+    public static List<FilteredDataItem> convertItems(List<Events> eventsList) {
+        List<FilteredDataItem>  list = new ArrayList<>();
+        for(Events events : eventsList) {
+            for (XmlEvent event : events.getEventList()) {
+                list.add(new FilteredDataItem(event));
+            }
+        }
+        return list;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getLogonType() {
+        return logonType;
+    }
+
+    public void setLogonType(String logonType) {
+        this.logonType = logonType;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+    
+    
+}
