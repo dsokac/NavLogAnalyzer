c520aa8b0eafa77149852f2605f8d62c8051d632
danijelsokac92 <danijel.sokac92@gmail.com>
2020-08-24 09:21:26 +0200

    use new class to simplify the objects with only relevant data

--- src/navloganalyzer/tasks/RemoveIrrelevantElementsTask.java
+++ src/navloganalyzer/tasks/RemoveIrrelevantElementsTask.java
@@ -5,23 +5,31 @@
  */
 package navloganalyzer.tasks;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.SwingWorker;
 import navloganalyzer.AppConstants;
 import navloganalyzer.models.Events;
+import navloganalyzer.models.FilteredDataItem;
 import navloganalyzer.models.XmlEvent;
+import navloganalyzer.utils.FilesUtils;
 
 /**
  *
  * @author DanijelSokac
  */
-public class RemoveIrrelevantElementsTask extends SwingWorker<List<Events>, Object>{
+public class RemoveIrrelevantElementsTask extends SwingWorker<List<FilteredDataItem>, Object>{
 
     private List<Events> eventsList = null;
     private Listener listener;
     private int total = 0;
     private int current = 0;
+    private List<FilteredDataItem> processedItems = new ArrayList<>();
     
     public RemoveIrrelevantElementsTask(Listener listener, List<Events> eventsList) {
         this.eventsList = eventsList;
@@ -29,33 +37,48 @@ public class RemoveIrrelevantElementsTask extends SwingWorker<List<Events>, Obje
     }
     
     @Override
-    protected List<Events> doInBackground() throws Exception {
+    protected List<FilteredDataItem> doInBackground() throws Exception {
         System.out.println("navloganalyzer.tasks.RemoveIrrelevantElementsTask.doInBackground()");
         this.listener.onTaskStarted("Removing irrelevant elements from memory...");
-        List<Events> processedEventsList = new ArrayList<>();
         for(Events events : eventsList) {
             total += events.getEventList().size();
         }
+        List<Events> processedEventsList = new ArrayList<>();
         for(Events events : eventsList) {
             Events processedEvents = new Events();
             List<XmlEvent> eventList = new ArrayList<>();
             for(XmlEvent event : events.getEventList()) {
                 this.updateProgressStatus();
-                if(event.getEventData() != null && event.getEventData().getTargetUserName().startsWith("student")) {
+                if(event != null && event.getEventData() != null && event.getEventData().getTargetUserName().startsWith("student")) {
                     eventList.add(event);
                 }
             }
             processedEvents.setEventList(eventList);
             processedEventsList.add(processedEvents);
         }
-        
-        return processedEventsList;
+        processedItems = FilteredDataItem.convertItems(processedEventsList);
+        return processedItems;
     }
     
     @Override
     protected void done() {
         System.out.println("navloganalyzer.tasks.RemoveIrrelevantElementsTask.done()");    
-        this.listener.onTaskFinished(AppConstants.Tasks.REMOVE_ELEMENTS_TASK, eventsList);
+        this.listener.onTaskFinished(AppConstants.Tasks.REMOVE_ELEMENTS_TASK, processedItems);
+        List<String> lines = new ArrayList<>();
+        for(FilteredDataItem item : processedItems) {
+            String output =  String.format("%s - %s - %d - %s", 
+                                item.getUsername(), 
+                                item.getCreatedAt(), 
+                                item.getEventId(),
+                                item.getLogonType()
+                        );
+            lines.add(output);
+        }
+        try {
+            FilesUtils.writeToFile(FilesUtils.getUserWorkingDir(), "logAnalysis.txt", lines, StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            Logger.getLogger(RemoveIrrelevantElementsTask.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     private void updateProgressStatus() {
@@ -66,7 +89,7 @@ public class RemoveIrrelevantElementsTask extends SwingWorker<List<Events>, Obje
     
     public interface Listener {
         void onTaskStarted(String taskDescription);
-        void onTaskFinished(String taskName, List<Events> eventsList);
+        void onTaskFinished(String taskName, Object filteredItems);
         void onProgressStatusChanged(int progress);
     }
     
